86ef2194f5823d148b16fb287b532ba894e0c8a1
cosmin <cosminamarandei14@gmail.com>
2023-10-19 17:28:14 +0300

    first commit

---
+++ Proiect_IS/src/main/java/com/example/TestProiectBackend/TestProiectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestProiectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProiectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProiectBackendApplication.class, args);
+	}
+
+}
